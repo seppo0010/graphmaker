7268dc92762173b3e4fb325bb6c9536d2e09d852
Sebastian Waisbrot <seppo0010@gmail.com>
2021-08-25 07:42:07 -0300

    Keep from when adding edge, auto change to to when adding from

--- src/EdgesView.tsx
+++ src/EdgesView.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useHotkeys } from 'react-hotkeys-hook';
 import Typography from '@material-ui/core/Typography';
@@ -13,6 +13,9 @@ import EdgeView from './EdgeView'
 import { Node, Edge, addEdge } from './graphSlice'
 import type { RootState } from './store'
 
+const From = 'From'
+const To = 'To'
+
 function EdgesView() {
   const dispatch = useDispatch()
   const graph = useSelector((state: RootState) => state.graph)
@@ -20,14 +23,29 @@ function EdgesView() {
   const [searchCriteria, setSearchCriteria] = useState('')
   const [newEdgeFrom, setNewEdgeFrom] = useState<Node | null>(null)
   const [newEdgeTo, setNewEdgeTo] = useState<Node | null>(null)
-  useEffect(() => {
-    if (!newEdgeFrom || !newEdgeTo) return
-    dispatch(addEdge({'from': newEdgeFrom, to: newEdgeTo}))
-    setNewEdgeFrom(null)
+  const [version, setVersion] = useState(1)
+  const updateEdge = (field: typeof From | typeof To, value: Node | null) => {
+    const f = field === From ? value : newEdgeFrom
+    const t = field === To ? value : newEdgeTo
+    if (f && !t) {
+      (newEdgeToRef?.current?.getElementsByTagName('INPUT')[0] as HTMLInputElement).focus()
+    }
+    if (!f || !t) {
+      console.log(field, value)
+      if (field === From) setNewEdgeFrom(value)
+      if (field === To) setNewEdgeTo(value)
+      return
+    }
+    dispatch(addEdge({'from': f, to: t}))
     setNewEdgeTo(null)
-  }, [setNewEdgeFrom, setNewEdgeTo, newEdgeFrom, newEdgeTo, dispatch])
+    setVersion(version+1);
+    setTimeout(() => {
+      (newEdgeFromRef?.current?.getElementsByTagName('INPUT')[0] as HTMLInputElement).focus()
+    })
+  }
 
-  const addRef = useRef<HTMLInputElement>(null)
+  const newEdgeFromRef = useRef<HTMLInputElement>(null)
+  const newEdgeToRef = useRef<HTMLInputElement>(null)
   const searchRef = useRef<HTMLInputElement>(null)
   useHotkeys('esc', () => {
     const el = (document.activeElement as any)
@@ -38,7 +56,7 @@ function EdgesView() {
     el.value = v
   }, {enableOnTags: ['INPUT']})
   useHotkeys('a', () => {
-    (addRef?.current?.getElementsByTagName('INPUT')[0] as HTMLInputElement).focus()
+    (newEdgeFromRef?.current?.getElementsByTagName('INPUT')[0] as HTMLInputElement).focus()
   }, {keyup: true})
   useHotkeys('s', (e: KeyboardEvent) => !e.ctrlKey && searchRef?.current?.focus(), {keyup: true})
   const edges = searchCriteria === '' ? graph.edges : graph.edges.filter((e: Edge) => nodesById[e.from].label.indexOf(searchCriteria) > -1 || nodesById[e.to].label.indexOf(searchCriteria) > -1)
@@ -76,12 +94,15 @@ function EdgesView() {
         }}
       />
       <Autocomplete
+        key={From + version}
         options={graph.nodes.slice().reverse()}
         getOptionLabel={(option) => option.label}
-        onChange={(_, value) => setNewEdgeFrom(value)}
+        onChange={(_, value, reason) => reason === 'select-option' && updateEdge(From, value)}
+        autoSelect={true}
+        clearOnBlur={false}
         value={newEdgeFrom}
         blurOnSelect
-        ref={addRef}
+        ref={newEdgeFromRef}
         renderInput={(params) => (
           <TextField
             {...params}
@@ -91,10 +112,14 @@ function EdgesView() {
           )}
         />
       <Autocomplete
+        key={To + version}
         options={graph.nodes.slice().reverse()}
         getOptionLabel={(option) => option.label}
         blurOnSelect
-        onChange={(_, value) => setNewEdgeTo(value)}
+        ref={newEdgeToRef}
+        onChange={(_, value, reason) => reason === 'select-option' && updateEdge(To, value)}
+        autoSelect={true}
+        clearOnBlur={false}
         value={newEdgeTo}
         renderInput={(params) => (
           <TextField
